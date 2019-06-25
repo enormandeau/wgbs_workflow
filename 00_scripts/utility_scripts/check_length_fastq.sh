ca9f6566789d428243ca3cec2633050eed0b4caa
Eric Normandeau <eric.normandeau.qc@gmail.com>
2019-06-25 13:56:43 -0400

    Minor: format

--- 00_scripts/utility_scripts/check_length_fastq.sh
+++ 00_scripts/utility_scripts/check_length_fastq.sh
@@ -15,11 +15,10 @@ cd $SLURM_SUBMIT_DIR
 for i in $(ls 03_trimmed/*fq)
 
 do
-
-wc -l $i >> file.size.temp
+    wc -l $i >> file.size.temp
 done
 
-awk '{print $1/4,$2}' file.size.temp >size_file_trimmed.txt
+awk '{print $1/4,$2}' file.size.temp > size_file_trimmed.txt
 
 # clean up
 
